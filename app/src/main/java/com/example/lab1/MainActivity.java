422492206df1f03fc609f95b2c8fdc49f76722e5
lukasmisiukevicius <misiukeviciuslukas@yahoo.com>
2022-09-23 09:40:45 +0300

    Added useless comment line

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -24,4 +24,6 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnChangeTextColorClick(View view) {
         tvMain.setTextColor(Color.RED);
     }
+
+    // This is going to be reverted
 }
